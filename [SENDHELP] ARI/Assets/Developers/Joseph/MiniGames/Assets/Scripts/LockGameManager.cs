c1cc8ab98b88e78ca03c7caf105faf9dfcc929f6
Matthew Medina <matthewmedina0706@gmail.com>
2022-11-07 14:56:51 -0500

    Fixed Lockpicking game
    Ready to implement into locked cabinet for Level 1

--- [SENDHELP] ARI/Assets/Developers/Joseph/MiniGames/Assets/Scripts/LockGameManager.cs
+++ [SENDHELP] ARI/Assets/Developers/Joseph/MiniGames/Assets/Scripts/LockGameManager.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LockGameManager : MonoBehaviour
 {
 
     public LockGameData GameData;
     public LockGameEvent OnWinEvent;
-
+    public string SceneToLoad;
+    public float delayTime = 3f;
     bool isFirstTap = true;
-
+    bool won = false;
     void Start()
     {
         GameData.ResetLevel();
@@ -27,10 +29,13 @@ public class LockGameManager : MonoBehaviour
 
     void CheckGoalsLeft()
     {
+        if(GameData.GoalsLeft == 0 && !won) {
+            won = true;
+            Debug.Log("WON BOOL SET");
+        }
         if (GameData.GoalsLeft <= 0)
         {
             OnWinEvent.Raise();
-	    Debug.Log("TIME ELAPSED: " + Time.time);
             StopLevel();
         }
     }
@@ -43,6 +48,19 @@ public class LockGameManager : MonoBehaviour
     public void StopLevel()
     {
         GameData.isRunning = false;
-//exit scene here
+        if(won) {
+            Invoke("nextScene", delayTime);
+        }
+        else {
+            Debug.Log(won + " - RELOADING");
+            Invoke("reload", 1f);
+        }
+    }
+
+    public void reload() {
+        SceneManager.LoadScene("Lockpicking");
+    }
+    public void nextScene() {
+        SceneManager.LoadScene(SceneToLoad);
     }
 }
